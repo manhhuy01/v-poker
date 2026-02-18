8a1a55944814ec2e11384ace124b5d4d7e39f6ec
HuyNguyen <huy.nguyen@vexere.com>
2026-02-18 16:12:01 +0700

    fix card

--- components/materials/card.js
+++ components/materials/card.js
@@ -55,7 +55,7 @@ export default function card({ data = 'u', isFold, isHidden }) {
   }
   return (
     <div className={` ${hide ? '' : 'transform-180'} duration-500 transition-transform transform-3d relative transform-gpu w-8 h-10 md:w-10 md:h-14 ml-1 mr-1`}>
-      <div className={` backface-hidden transform-180 ${colorClass} ${isFold ? 'bg-opacity-50' : ''} w-full h-full absolute border-black bg-white shadow rounded  font-bold p-1 `}>
+      <div className={` backface-hidden transform-180 ${colorClass} ${isFold ? 'bg-opacity-50' : ''} w-full h-full absolute border-black bg-white shadow rounded  font-bold p-1 flex flex-col justify-evenly`}>
         <div className="leading-4 md:text-xl">{num}</div>
         <div className="text-lg md:text-2xl leading-4 md:leading-4 text-right ">{symbol}</div>
       </div>
